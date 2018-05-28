6c5b0f8969afe21ebad653316ccd041d352259ea
Wim Looman <wim@nemo157.com>
2018-05-28 12:02:49 +0200

    Add helpers to convert action type

--- src/node.rs
+++ src/node.rs
@@ -23,3 +23,37 @@ pub struct VTag<A> {
     pub key: Option<String>,
     pub namespace: Option<String>,
 }
+
+impl<A> VProperty<A> {
+    pub fn map_action<B>(self, f: &impl Fn(A) -> B) -> VProperty<B> {
+        match self {
+            VProperty::Action(a) => VProperty::Action(f(a)),
+            VProperty::Text(t) => VProperty::Text(t),
+            VProperty::Object(o) => VProperty::Object(o),
+        }
+    }
+}
+
+impl<A> VNode<A> {
+    pub fn map_action<B>(self, f: &impl Fn(A) -> B) -> VNode<B> {
+        match self {
+            VNode::Tag(t) => VNode::Tag(t.map_action(f)),
+            VNode::Text(t) => VNode::Text(t),
+        }
+    }
+}
+
+impl<A> VTag<A> {
+    pub fn map_action<B>(self, f: &impl Fn(A) -> B) -> VTag<B> {
+        let VTag { name, properties, children, key, namespace } = self;
+        let properties = properties
+            .into_iter()
+            .map(|(k, v)| (k, v.map_action(f)))
+            .collect();
+        let children = children
+            .into_iter()
+            .map(|c| c.map_action(f))
+            .collect();
+        VTag { name, properties, children, key, namespace }
+    }
+}
